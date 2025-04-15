c556f6e94d4b8014c357217843a8cb0ca4a09b88
Mark Karman <mkarman08@gmail.com>
2025-04-15 16:21:07 -0700

    adding new configuration

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,10 @@
 vim.keymap.set("n","-","<cmd>Oil --float<CR>",{desc = "Open Parent Directory in Oil"})
 
+-- highlight when yanked.
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
